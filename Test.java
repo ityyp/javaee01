a5687496a461827ccc7cf2c96097f09e7568d10e
ityyp <1142969055@qq.com>
2023-11-14 21:18:08 +0800

    v1

---
+++ Test.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ArrayList<String> array = new ArrayList<String>();
+
+        //添加元素
+        array.add("hello");
+        array.add("world");
+        array.add("java");
+
+        System.out.println(array.get(3)); //？？？？？？ 自己测试
+
+    }
+}
---
+++ Worker.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Worker {
+    private String name;
+    private int year;
+
+    public Worker() {
+    }
+
+    public Worker(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void show(){
+        System.out.println(name+","+year);
+    }
+}
